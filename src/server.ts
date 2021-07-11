1280f7bf7b4922ca6a609bc5f4720f97f44e16cf
omoriyasuo <omoriyasuo.dev@gmail.com>
2021-07-11 21:17:21 +0900

    🏗️ setup basic architecture

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,19 @@
 import fastify from 'fastify';
 
-const server = fastify({
-  logger: true,
-});
+const main = () => {
+  const server = fastify({
+    logger: true,
+  });
 
-server.get('/', async (request, reply) => {
-  reply.type('application/json').code(200);
-  return { hello: 'world' };
-});
+  server.get('/', async (request, reply) => {
+    reply.type('application/json').code(200);
+    return { hello: 'world' };
+  });
 
-server.listen(3000, (error, address) => {
-  if (error) throw error;
-  server.log.info(`server listening on ${address}`);
-});
+  server.listen(3000, (error, address) => {
+    if (error) throw error;
+    server.log.info(`🛡️  Server listening on port: ${address} 🛡️`);
+  });
+};
+
+main();
